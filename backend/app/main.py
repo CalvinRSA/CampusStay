007aa7060ae649fca8fb2b49cac23c2501fb64d1
CalvinRSA <molakengcalvin@gmail.com>
2026-01-19 15:46:06 +0200

    Update CORS settings to remove localhost origins
    Removed localhost origins from CORS settings

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,8 +22,6 @@ app.add_middleware(
         "https://campusstay.co.za",
         "https://www.campusstay.co.za",
         "http://campusstay.co.za",
-        "http://localhost:5173",
-        "http://localhost:3000",
     ],
     allow_credentials=True,               
     allow_methods=["*"],
